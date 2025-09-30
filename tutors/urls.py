6f8562c41cb5c9d251afa2eff5668398362a4a77
Neelp20 <8f5sdjz8@students.codeinstitute.net>
2025-09-30 13:35:24 +0100

    Add header/footer templates

---
+++ tutors/urls.py
@@ -0,0 +1,9 @@
+# from django.urls import path 
+# from . import views
+
+# urlpatterns = [
+#     path('create_tutor', views.create_tutor, name='create_tutor'),
+#     path('edit_tutor', views.edit_tutor, name='edit_tutor'),
+#     path('tutor_detail', views.tutor_detail, name='tutor_detail'),
+#     path('tutors_list', views.tutor_list, name='tutor_list'),
+# ]
