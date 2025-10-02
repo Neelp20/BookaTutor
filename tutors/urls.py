783d1d1a246a6d198a3cbb1dd2e7f49eb5f2c373
Neelp20 <8f5sdjz8@students.codeinstitute.net>
2025-10-02 11:40:13 +0100

    Wire up the tutors views in urls

--- tutors/urls.py
+++ tutors/urls.py
@@ -1,6 +1,13 @@
-from django.urls import path 
-from . import views
+from django.urls import path
+from . import TutorListView, TutorDetailView, TutorCreateView
+from . import TutorUpdateView, TutorDeleteView
+
 
 urlpatterns = [
-    path('', views.tutors_list, name='tutors_list'),
+    # path('', views.tutors_list, name='tutors_list'),
+    path('', TutorListView.as_view(), name='tutors-list'),
+    path('<int:pk>/', TutorDetailView.as_view(), name='tutor-detail'),
+    path('create/', TutorCreateView.as_view(), name='create-tutor'),
+    path('edit/<int:pk>/', TutorUpdateView.as_view(), name='update-tutor'),
+    path('delete/<int:pk>/', TutorDeleteView.as_view(), name='delete-tutor'),
 ]
