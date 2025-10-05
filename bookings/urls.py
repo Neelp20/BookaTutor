31d9e98de2e79b198336c8d8c6098d145db8770e
Neelp20 <8f5sdjz8@students.codeinstitute.net>
2025-10-05 16:48:55 +0100

    Add bookings template

--- bookings/urls.py
+++ bookings/urls.py
@@ -1,8 +1,26 @@
-from django.contrib import admin
+# from django.contrib import admin
+# from django.urls import path
+# from . import views
+
+# urlpatterns = [
+#     path('', views.Booking, name='bookings')
+    
+# ]
+
 from django.urls import path
-from . import views
+from .views import (
+    BookingListView, PastBookingListView, BookingCreateView,
+    BookingUpdateView, BookingDeleteView, AdminBookingListView,
+    BookingCalendarView
+)
 
 urlpatterns = [
-    path('', views.bookings, name='bookings')
-    
+    path('', BookingCreateView.as_view(), name='bookings'),
+    path('manage/', BookingListView.as_view(), name='manage-bookings'),
+    path('past/', PastBookingListView.as_view(), name='past-bookings'),
+    path('edit/<int:pk>/', BookingUpdateView.as_view(), name='edit-booking'),
+    path('delete/<int:pk>/', BookingDeleteView.as_view(), name='delete-booking'),
+    path('admin/manage/', AdminBookingListView.as_view(), name='admin-manage-bookings'),
+    path('calendar/', BookingCalendarView.as_view(), name='booking-calendar'),
 ]
+
--- bookings/views.py
+++ bookings/views.py
@@ -1,9 +1,122 @@
-from django.shortcuts import render
+# from django.shortcuts import render
 
 # Create your views here.
 
 
-def bookings(request):
-    """ A view to return the bookings page """
+# def bookings(request):
+#     """ A view to return the bookings page """
 
-    return render(request, 'bookings/bookings.html')
+#     return render(request, 'bookings/bookings.html')
+
+from django.shortcuts import render
+from django.views.generic import ListView, CreateView, UpdateView
+from django.views.generic import DeleteView, TemplateView
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+from django.urls import reverse_lazy
+from django.utils import timezone
+from .models import Booking, TimeSlot
+from .forms import BookingForm
+
+
+class BookingListView(LoginRequiredMixin, ListView):
+    model = Booking
+    template_name = 'bookings/manage_bookings.html'
+    context_object_name = 'bookings'
+
+    def get_queryset(self):
+        return Booking.objects.filter(
+            student=self.request.user,
+            timeslot__date__gte=timezone.now().date()
+            )
+
+
+class PastBookingListView(LoginRequiredMixin, ListView):
+    model = Booking
+    template_name = 'bookings/past_bookings.html'
+    context_object_name = 'bookings'
+
+    def get_queryset(self):
+        return Booking.objects.filter(
+            student=self.request.user, 
+            timeslot__date__lt=timezone.now().date()
+            )
+
+
+class BookingCreateView(LoginRequiredMixin, CreateView):
+    model = Booking
+    form_class = BookingForm
+    template_name = 'bookings/bookings.html'
+    success_url = reverse_lazy('manage-bookings')
+
+    def get_form_kwargs(self):
+        kwargs = super().get_form_kwargs()
+        kwargs['user'] = self.request.user
+        return kwargs
+
+    def get_initial(self):
+        initial = super().get_initial()
+        tutor_id = self.request.GET.get('tutor')
+        slot_id = self.request.GET.get('slot')
+        if tutor_id:
+            initial['tutor'] = tutor_id
+        if slot_id:
+            initial['timeslot'] = slot_id
+        return initial
+
+    def form_valid(self, form):
+        form.instance.student = self.request.user
+        return super().form_valid(form)
+
+
+class BookingUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
+    model = Booking
+    form_class = BookingForm
+    template_name = 'bookings/bookings.html'
+    success_url = reverse_lazy('manage-bookings')
+
+    def get_form_kwargs(self):
+        kwargs = super().get_form_kwargs()
+        kwargs['user'] = self.request.user
+        return kwargs
+
+    def test_func(self):
+        return self.get_object().student == self.request.user
+
+
+class BookingDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
+    model = Booking
+    template_name = 'bookings/confirm_delete_booking.html'
+    success_url = reverse_lazy('manage-bookings')
+
+    def test_func(self):
+        return self.get_object().student == self.request.user
+
+
+class AdminBookingListView(LoginRequiredMixin, UserPassesTestMixin, ListView):
+    model = Booking
+    template_name = 'bookings/admin_manage_bookings.html'
+    context_object_name = 'bookings'
+
+    def test_func(self):
+        return self.request.user.is_superuser
+
+
+class BookingCalendarView(LoginRequiredMixin, TemplateView):
+    template_name = 'bookings/calendar.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        timeslots = TimeSlot.objects.filter(
+            date__gte=timezone.now().date()).order_by('date', 'start_time'
+                                                      )
+        events = []
+        for slot in timeslots:
+            events.append({
+                'title': f"{slot.tutor.user.get_full_name()}",
+                'start': f"{slot.date}T{slot.start_time}",
+                'end': f"{slot.date}T{slot.end_time}",
+                'url': f"/bookings/?tutor={slot.tutor.id}&slot={slot.id}",
+                'color': '#28a745' if slot.bookings.count() == 0 else '#dc3545',
+            })
+        context['events'] = events
+        return context
