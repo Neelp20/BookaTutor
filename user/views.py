b236c2c59b0a1288e52e7884f353fa86cf0d3155
Neelp20 <8f5sdjz8@students.codeinstitute.net>
2025-10-07 14:13:11 +0100

    Update user app's views.py

--- user/views.py
+++ user/views.py
@@ -1,26 +1,24 @@
-from django.contrib.auth.decorators import login_required
-from django.utils.decorators import method_decorator
 from django.views.generic import DetailView, UpdateView
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.urls import reverse_lazy
 from .models import UserProfile
 from .forms import UserProfileForm
 
 
-@method_decorator(login_required, name='dispatch')
-class ProfileDetailView(DetailView):
+class ProfileDetailView(LoginRequiredMixin, DetailView):
     model = UserProfile
     template_name = 'users/profile_detail.html'
+    context_object_name = 'profile'
 
-    def get_object(self):
+    def get_object(self, queryset=None):
         return self.request.user.userprofile
 
 
-@method_decorator(login_required, name='dispatch')
-class ProfileEditView(UpdateView):
+class ProfileUpdateView(LoginRequiredMixin, UpdateView):
     model = UserProfile
     form_class = UserProfileForm
     template_name = 'users/profile_edit.html'
     success_url = reverse_lazy('profile-detail')
 
-    def get_object(self):
+    def get_object(self, queryset=None):
         return self.request.user.userprofile
