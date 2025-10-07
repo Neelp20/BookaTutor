a18ffd3ae3d558f5de5191a5ba189628cfd87680
Neelp20 <8f5sdjz8@students.codeinstitute.net>
2025-10-07 14:06:02 +0100

    Add widgets to user's form

--- user/forms.py
+++ user/forms.py
@@ -5,4 +5,9 @@ from .models import UserProfile
 class UserProfileForm(forms.ModelForm):
     class Meta:
         model = UserProfile
-        fields = ['phone', 'address', 'city', 'country', 'bio']
+        fields = ['phone', 'address', 'city', 'country', 'bio', 'profile_image']
+        widgets = {
+            'bio': forms.Textarea(attrs={'rows': 3, 'class': 'form-control'}),
+            'phone': forms.TextInput(attrs={'class': 'form-control'}),
+            'role': forms.Select(attrs={'class': 'form-select'}),
+        }
