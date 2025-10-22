e312712898ec72c587e15bb1acb23d67ab9708e2
Neelp20 <8f5sdjz8@students.codeinstitute.net>
2025-10-22 17:16:02 +0100

    Customise the allauth pages

--- user/admin.py
+++ user/admin.py
@@ -2,3 +2,9 @@ from django.contrib import admin
 from .models import UserProfile
 
 admin.site.register(UserProfile)
+
+
+class UserProfileAdmin(admin.ModelAdmin):
+    # Disable the "Add" button for existing users
+    def has_add_permission(self, request):
+        return False  # Only allow editing
