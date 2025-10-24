58e54be6ae47bb899784f5f5508038e0dbe1859a
Neelp20 <8f5sdjz8@students.codeinstitute.net>
2025-10-24 12:38:18 +0100

    Add newsletter in the footer

--- home/urls.py
+++ home/urls.py
@@ -3,5 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='home')
+    path('', views.index, name='home'),
+    path('subscribe/', views.subscribe_newsletter, name='subscribe-newsletter'),
+    
 ]
--- home/views.py
+++ home/views.py
@@ -1,4 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib import messages
+from .forms import NewsletterForm
 
 # Create your views here.
 
@@ -7,3 +9,15 @@ def index(request):
     """ A view to return the index page """
 
     return render(request, 'home/index.html')
+
+
+def subscribe_newsletter(request):
+    """ Handle newsletter form submission from footer """
+    if request.method == 'POST':
+        form = NewsletterForm(request.POST)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Thank you for subscribing!')
+        else:
+            messages.error(request, 'This email is already subscribed or invalid.')
+    return redirect(request.META.get('HTTP_REFERER', 'home'))
