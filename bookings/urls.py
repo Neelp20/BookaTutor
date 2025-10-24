f472a0ff73033c8ed709b0df72b0992f0148f678
Neelp20 <8f5sdjz8@students.codeinstitute.net>
2025-10-24 16:31:08 +0100

    Create time slots for tutors

--- bookings/urls.py
+++ bookings/urls.py
@@ -1,26 +1,11 @@
-# from django.contrib import admin
-# from django.urls import path
-# from . import views
-
-# urlpatterns = [
-#     path('', views.Booking, name='bookings')
-    
-# ]
-
 from django.urls import path
-from .views import (
-    BookingListView, PastBookingListView, BookingCreateView,
-    BookingUpdateView, BookingDeleteView, AdminBookingListView,
-    BookingCalendarView
-)
+from . import views
 
 urlpatterns = [
-    path('', BookingCreateView.as_view(), name='bookings'),
-    path('manage/', BookingListView.as_view(), name='manage-bookings'),
-    path('past/', PastBookingListView.as_view(), name='past-bookings'),
-    path('edit/<int:pk>/', BookingUpdateView.as_view(), name='edit-booking'),
-    path('delete/<int:pk>/', BookingDeleteView.as_view(), name='delete-booking'),
-    path('admin/manage/', AdminBookingListView.as_view(), name='admin-manage-bookings'),
-    path('calendar/', BookingCalendarView.as_view(), name='booking-calendar'),
+    path('', views.BookingCreateView.as_view(), name='bookings'),
+    path('manage/', views.BookingListView.as_view(), name='manage-bookings'),
+    path('edit/<int:pk>/', views.BookingUpdateView.as_view(), name='edit-booking'),
+    path('delete/<int:pk>/', views.BookingDeleteView.as_view(), name='delete-booking'),
+    path('past/', views.PastBookingListView.as_view(), name='past-bookings'),
+    path('admin/manage/', views.AdminBookingListView.as_view(), name='admin-manage-bookings'),
 ]
-
