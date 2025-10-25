5fe152449d085548dcb7f1371cce07dc72d647eb
Neelp20 <8f5sdjz8@students.codeinstitute.net>
2025-10-25 10:50:08 +0100

    Remove the Become a tutor from navbar for student/user

--- user/forms.py
+++ user/forms.py
@@ -5,9 +5,12 @@ from .models import UserProfile
 class UserProfileForm(forms.ModelForm):
     class Meta:
         model = UserProfile
-        fields = ['phone', 'address', 'city', 'country']
+        fields = [
+            'phone', 'address', 'city', 'country']
         widgets = {
-            'bio': forms.Textarea(attrs={'rows': 3, 'class': 'form-control'}),
+            'bio': forms.Textarea(
+                attrs={'rows': 3,
+                       'placeholder': 'Write something about yourself...'}),
             'phone': forms.TextInput(attrs={'class': 'form-control'}),
-            'role': forms.Select(attrs={'class': 'form-select'}),
+
         }
