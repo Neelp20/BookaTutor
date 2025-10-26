165e5bea580e59e98f0687fe6cdea411b3511c9e
Neelp20 <8f5sdjz8@students.codeinstitute.net>
2025-10-26 10:31:57 +0000

    setting up stripe integration

--- checkout/urls.py
+++ checkout/urls.py
@@ -3,7 +3,8 @@ from django.views.generic import TemplateView
 
 urlpatterns = [
     path('success/', TemplateView.as_view(
-        template_name='checkout/payment_success.html'), name='payment_success'),
+        template_name='checkout/payment_success.html'),
+          name='payment_success'),
     path('cancel/', TemplateView.as_view(
         template_name='checkout/payment_cancel.html'), name='payment_cancel'),
 ]
--- checkout/views.py
+++ checkout/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
+from django.conf import settings
+import stripe
+stripe.api_key = settings.STRIPE_SECRET_KEY
 
 # Create your views here.
