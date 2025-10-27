10bc0e970ba9ea0741aa18f78cb71ec9e8b7ae6b
Neelp20 <8f5sdjz8@students.codeinstitute.net>
2025-10-27 15:01:15 +0000

    Temporarily disable user profile signal for data load

--- user/signals.py
+++ user/signals.py
@@ -5,14 +5,14 @@ from django.contrib.auth.models import User
 from .models import UserProfile
 
 
-@receiver(post_save, sender=User)
-def create_or_update_user_profile(sender, instance, created, **kwargs):
-    """
-    Create or update UserProfile whenever a User is created or saved.
-    Ensures no 'User has no userprofile' errors.
-    """
-    if created:
-        UserProfile.objects.create(user=instance)
-    else:
+# @receiver(post_save, sender=User)
+# def create_or_update_user_profile(sender, instance, created, **kwargs):
+#     """
+#     Create or update UserProfile whenever a User is created or saved.
+#     Ensures no 'User has no userprofile' errors.
+#     """
+#     if created:
+#         UserProfile.objects.create(user=instance)
+#     else:
         # Safely handle existing users that might not have a profile yet
-        UserProfile.objects.get_or_create(user=instance)
+        # UserProfile.objects.get_or_create(user=instance)
